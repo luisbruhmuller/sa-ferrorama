faac95643330e3898050d087a2f19478b0da6af1
arthurdmachadi <arthur_d_machado@estudante.sesisenai.org.br>
2026-09-17 12:53:51 -0300

    add - fazendo o mapa

---
+++ public/tela_mapa/tela_mapa.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=], initial-scale=1.0">
+    <title>GordoSensores -Tela Mapa</title>
+     <link rel="stylesheet" href="../../styles/style.css">
+</head>
+<body>
+
+     <div class="trilho">
+        <div class="trilho-interno"></div>
+    </div>
+     
+</body>
+</html>
